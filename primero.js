41a0b9be34ff93f21968f2b1bbd327e1b10f262c
mabecol <maurycyo@gmail.com>
2019-01-08 02:15:16 -0500

    primer commit

---
+++ primero.js
@@ -0,0 +1,17 @@
+
+function saludo(){
+var gato;
+gato = "Felino";
+alert(gato);
+console.log(gato);
+}
+
+function pagina(){
+let mensaje = "Mi primera Pagina";
+let div = `<div class="container">${mensaje} </div>`
+    document.body.innerHTML=div
+}
+function boton[
+
+    
+]
